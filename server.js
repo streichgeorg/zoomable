ab0ec7a9f7bb04bd4c66bf34e3bee0854eefadb1
Georg Streich <streichg@ethz.ch>
2021-12-18 17:27:52 -0500

    Some changes

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const app = express()
 
 app.use(express.static(path.join(__dirname, "/pub")));
 
-
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Listening on port ${port}...`);
